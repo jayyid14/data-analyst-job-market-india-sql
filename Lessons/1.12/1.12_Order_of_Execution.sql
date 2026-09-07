324b69481cbf0c719a92873af2aeba2b22620992
Jayyid Razi Ahmed <325701411+jayyid14@users.noreply.github.com>
2026-09-07 19:24:20 +0530

    Update Order of Execution Query

--- Lessons/1.12/1.12_Order_of_Execution.sql
+++ Lessons/1.12/1.12_Order_of_Execution.sql
@@ -1,8 +1,10 @@
 /*
 Find the top 10 companies for posting jobs.
 They must have >3000 postings
+Limit this only to US jobs
 
 */
+
 SELECT 
     cd.name AS company_name,
     COUNT(*) AS job_count
@@ -10,6 +12,8 @@ FROM
     job_postings_fact AS jpf
 LEFT JOIN company_dim AS cd
     ON jpf.company_id = cd.company_id
+WHERE
+    jpf.job_country = 'United States'
 GROUP BY
     cd.name
 HAVING
@@ -17,3 +21,40 @@ HAVING
 ORDER BY 
     COUNT(*) DESC;
 
+
+
+EXPLAIN
+SELECT 
+    cd.name AS company_name,
+    COUNT(*) AS job_count
+FROM 
+    job_postings_fact AS jpf
+LEFT JOIN company_dim AS cd
+    ON jpf.company_id = cd.company_id
+WHERE
+    jpf.job_country = 'United States'
+GROUP BY
+    cd.name
+HAVING
+    COUNT(*) > 3000
+ORDER BY 
+    COUNT(*) DESC;
+
+
+
+EXPLAIN ANALYZE
+SELECT 
+    cd.name AS company_name,
+    COUNT(*) AS job_count
+FROM 
+    job_postings_fact AS jpf
+LEFT JOIN company_dim AS cd
+    ON jpf.company_id = cd.company_id
+WHERE
+    jpf.job_country = 'United States'
+GROUP BY
+    cd.name
+HAVING
+    COUNT(*) > 3000
+ORDER BY 
+    COUNT(*) DESC;
